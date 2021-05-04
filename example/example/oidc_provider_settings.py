35231dbddf51cc35bf2f0ae2d012181d47406566
peppelinux <giuseppe.demarco@unical.it>
2021-05-04 16:42:20 +0200

    configuration

--- example/example/oidc_provider_settings.py
+++ example/example/oidc_provider_settings.py
@@ -1,9 +1,9 @@
-OIDCOP_CONF = {
-  "port": 8000,
-  "domain": "127.0.0.1",
-  "server_name": "{domain}:{port}",
-  "base_url": "https://{domain}:{port}",
-  "key_def": [
+DOMAIN = "127.0.0.1"
+PORT = 8000
+SERVER_NAME = f"{DOMAIN}:{PORT}"
+
+
+OIDC_KEY_DEFS = [
     {
       "type": "RSA",
       "use": [
@@ -17,24 +17,208 @@ OIDCOP_CONF = {
         "sig"
       ]
     }
-  ],
-  "OIDC_KEYS": {
-    "private_path": "data/oidc_op/private/jwks.json",
-    "key_defs": [
-      {
-        "type": "RSA",
-        "use": [
-          "sig"
-        ]
-      },
-      {
-        "type": "EC",
-        "crv": "P-256",
-        "use": [
-          "sig"
+]
+
+OIDC_OP_AUTHZ = {
+    "class": "oidcop.authz.AuthzHandling",
+    "kwargs": {
+        "grant_config": {
+            "usage_rules": {
+                "authorization_code": {
+                    "supports_minting": ["access_token", "refresh_token", "id_token"],
+                    "max_usage": 1
+                },
+                "access_token": {},
+                "refresh_token": {
+                    "supports_minting": ["access_token", "refresh_token"]
+                }
+            },
+            "expires_in": 43200
+        }
+    }
+}
+
+
+OIDC_OP_ENDPOINTS = {
+    "webfinger": {
+      "path": ".well-known/webfinger",
+      "class": "oidcop.oidc.discovery.Discovery",
+      "kwargs": {
+        "client_authn_method": None
+      }
+    },
+    "provider_info": {
+      "path": ".well-known/openid-configuration",
+      "class": "oidcop.oidc.provider_config.ProviderConfiguration",
+      "kwargs": {
+        "client_authn_method": None
+      }
+    },
+    "registration": {
+      "path": "registration",
+      "class": "oidcop.oidc.registration.Registration",
+      "kwargs": {
+        "client_authn_method": None,
+        "client_secret_expiration_time": 432000
+      }
+    },
+    "registration_api": {
+      "path": "registration_api",
+      "class": "oidcop.oidc.read_registration.RegistrationRead",
+      "kwargs": {
+        "client_authn_method": [
+          "bearer_header"
         ]
       }
-    ],
+    },
+    "introspection": {
+      "path": "introspection",
+      "class": "oidcop.oauth2.introspection.Introspection",
+      "kwargs": {
+        "client_authn_method": [
+          "client_secret_post"
+        ],
+        "release": [
+          "username"
+        ]
+      }
+    },
+    "authorization": {
+      "path": "authorization",
+      "class": "oidcop.oidc.authorization.Authorization",
+      "kwargs": {
+        "client_authn_method": None,
+        "claims_parameter_supported": True,
+        "request_parameter_supported": True,
+        "request_uri_parameter_supported": True,
+        "response_types_supported": [
+          "code",
+          # "token",
+          # "id_token",
+          # "code token",
+          # "code id_token",
+          # "id_token token",
+          # "code id_token token",
+          # "none"
+        ],
+        "response_modes_supported": [
+          "query",
+          "fragment",
+          "form_post"
+        ]
+      }
+    },
+    "token": {
+      "path": "token",
+      "class": "oidcop.oidc.token.Token",
+      "kwargs": {
+        "client_authn_method": [
+          "client_secret_post",
+          "client_secret_basic",
+          "client_secret_jwt",
+          "private_key_jwt"
+        ]
+      }
+    },
+    "userinfo": {
+      "path": "userinfo",
+      "class": "oidcop.oidc.userinfo.UserInfo",
+      "kwargs": {
+        "claim_types_supported": [
+          "normal",
+          "aggregated",
+          "distributed"
+        ]
+      }
+    },
+    "end_session": {
+      "path": "session",
+      "class": "oidcop.oidc.session.Session",
+      "kwargs": {
+        "logout_verify_url": "verify_logout",
+        "post_logout_uri_path": "post_logout",
+        "signing_alg": "ES256",
+        "frontchannel_logout_supported": True,
+        "frontchannel_logout_session_supported": True,
+        "backchannel_logout_supported": True,
+        "backchannel_logout_session_supported": True,
+        "check_session_iframe": "check_session_iframe"
+      }
+    }
+}
+
+
+OIDC_OP_TOKEN_HANDLER = {
+    "jwks_def": {
+      "private_path": "data/oidc_op/private/token_jwks.json",
+      "read_only": False,
+      "key_defs": [
+        {
+          "type": "oct",
+          "bytes": 24,
+          "use": [
+            "enc"
+          ],
+          "kid": "code"
+        },
+        {
+          "type": "oct",
+          "bytes": 24,
+          "use": [
+            "enc"
+          ],
+          "kid": "refresh"
+        }
+      ]
+    },
+    "code": {
+      "kwargs": {
+        "lifetime": 600
+      }
+    },
+    "token": {
+      "class": "oidcop.token.jwt_token.JWTToken",
+      "kwargs": {
+        "lifetime": 3600,
+        # this will add email in access_token
+        "base_claims": {
+            "email": None,
+            "email_verified": None,
+        },
+        # "enable_claims_per_client": True,
+        # "aud": [
+          # "https://example.org/appl"
+        # ]
+      }
+    },
+    "refresh": {
+      "kwargs": {
+        "lifetime": 86400
+      }
+    }
+}
+
+
+OIDC_OP_IDTOKEN = {
+    "class": "oidcop.id_token.IDToken",
+    "kwargs": {
+        "base_claims": {
+            "email": None,
+            "email_verified": None,
+        },
+    },
+}
+
+
+OIDCOP_CONF = {
+  "port": PORT,
+  "domain": DOMAIN,
+  "server_name": SERVER_NAME,
+  "base_url": f"https://{SERVER_NAME}",
+  "key_def": OIDC_KEY_DEFS,
+  "OIDC_KEYS": {
+    "private_path": "data/oidc_op/private/jwks.json",
+    "key_defs": OIDC_KEY_DEFS,
     "public_path": "data/static/jwks.json",
     "read_only": False,
     "uri_path": "static/jwks.json"
@@ -62,28 +246,14 @@ OIDCOP_CONF = {
               "sub",
               "iss",
               "eduperson_scoped_affiliation"
-            ]
+            ],
+            # "profile": [
+              # "email"
+            # ]
           }
         }
       },
-      "authz": {
-        "class": "oidcop.authz.AuthzHandling",
-        "kwargs": {
-            "grant_config": {
-                "usage_rules": {
-                    "authorization_code": {
-                        "supports_minting": ["access_token", "refresh_token", "id_token"],
-                        "max_usage": 1
-                    },
-                    "access_token": {},
-                    "refresh_token": {
-                        "supports_minting": ["access_token", "refresh_token"]
-                    }
-                },
-                "expires_in": 43200
-            }
-        }
-      },
+      "authz": OIDC_OP_AUTHZ,
       "authentication": {
         "user": {
           "acr": "oidcop.user_authn.authn_context.INTERNETPROTOCOLPASSWORD",
@@ -114,7 +284,7 @@ OIDCOP_CONF = {
         # would get a 403 error message instead of be declined implicitly.
         # If False the op will only release the available scopes and ignoring the missings.
         # Default to False
-        #deny_unknown_scopes: True
+        # deny_unknown_scopes: True
       },
       "cookie_handler": {
         "class": "oidcop.cookie_handler.CookieHandler",
@@ -134,147 +304,15 @@ OIDCOP_CONF = {
           }
         }
       },
-      "endpoint": {
-        "webfinger": {
-          "path": ".well-known/webfinger",
-          "class": "oidcop.oidc.discovery.Discovery",
-          "kwargs": {
-            "client_authn_method": None
-          }
-        },
-        "provider_info": {
-          "path": ".well-known/openid-configuration",
-          "class": "oidcop.oidc.provider_config.ProviderConfiguration",
-          "kwargs": {
-            "client_authn_method": None
-          }
-        },
-        "registration": {
-          "path": "registration",
-          "class": "oidcop.oidc.registration.Registration",
-          "kwargs": {
-            "client_authn_method": None,
-            "client_secret_expiration_time": 432000
-          }
-        },
-        "registration_api": {
-          "path": "registration_api",
-          "class": "oidcop.oidc.read_registration.RegistrationRead",
-          "kwargs": {
-            "client_authn_method": [
-              "bearer_header"
-            ]
-          }
-        },
-        "introspection": {
-          "path": "introspection",
-          "class": "oidcop.oauth2.introspection.Introspection",
-          "kwargs": {
-            "client_authn_method": [
-              "client_secret_post"
-            ],
-            "release": [
-              "username"
-            ]
-          }
-        },
-        "authorization": {
-          "path": "authorization",
-          "class": "oidcop.oidc.authorization.Authorization",
-          "kwargs": {
-            "client_authn_method": None,
-            "claims_parameter_supported": True,
-            "request_parameter_supported": True,
-            "request_uri_parameter_supported": True,
-            "response_types_supported": [
-              "code",
-              "token",
-              "id_token",
-              "code token",
-              "code id_token",
-              "id_token token",
-              "code id_token token",
-              "none"
-            ],
-            "response_modes_supported": [
-              "query",
-              "fragment",
-              "form_post"
-            ]
-          }
-        },
-        "token": {
-          "path": "token",
-          "class": "oidcop.oidc.token.Token",
-          "kwargs": {
-            "client_authn_method": [
-              "client_secret_post",
-              "client_secret_basic",
-              "client_secret_jwt",
-              "private_key_jwt"
-            ]
-          }
-        },
-        "userinfo": {
-          "path": "userinfo",
-          "class": "oidcop.oidc.userinfo.UserInfo",
-          "kwargs": {
-            "claim_types_supported": [
-              "normal",
-              "aggregated",
-              "distributed"
-            ]
-          }
-        },
-        "end_session": {
-          "path": "session",
-          "class": "oidcop.oidc.session.Session",
-          "kwargs": {
-            "logout_verify_url": "verify_logout",
-            "post_logout_uri_path": "post_logout",
-            "signing_alg": "ES256",
-            "frontchannel_logout_supported": True,
-            "frontchannel_logout_session_supported": True,
-            "backchannel_logout_supported": True,
-            "backchannel_logout_session_supported": True,
-            "check_session_iframe": "check_session_iframe"
-          }
-        }
-      },
       "httpc_params": {
         "verify": False
       },
-      "id_token": {
-        "class": "oidcop.id_token.IDToken",
-        "kwargs": {
-          "default_claims": {
-            "email": {
-              "essential": True
-            },
-            "email_verified": {
-              "essential": True
-            }
-          }
-        }
-      },
-      "issuer": "https://{domain}:{port}",
+      "endpoint": OIDC_OP_ENDPOINTS,
+      "id_token": OIDC_OP_IDTOKEN,
+      "issuer": f"https://{SERVER_NAME}",
       "keys": {
         "private_path": "data/oidc_op/private/jwks.json",
-        "key_defs": [
-          {
-            "type": "RSA",
-            "use": [
-              "sig"
-            ]
-          },
-          {
-            "type": "EC",
-            "crv": "P-256",
-            "use": [
-              "sig"
-            ]
-          }
-        ],
+        "key_defs": OIDC_KEY_DEFS,
         "public_path": "data/static/jwks.json",
         "read_only": False,
         "uri_path": "static/jwks.json"
@@ -295,56 +333,7 @@ OIDCOP_CONF = {
         "use": "sig"
       },
       "template_dir": "templates",
-      "token_handler_args": {
-        "jwks_def": {
-          "private_path": "data/oidc_op/private/token_jwks.json",
-          "read_only": False,
-          "key_defs": [
-            {
-              "type": "oct",
-              "bytes": 24,
-              "use": [
-                "enc"
-              ],
-              "kid": "code"
-            },
-            {
-              "type": "oct",
-              "bytes": 24,
-              "use": [
-                "enc"
-              ],
-              "kid": "refresh"
-            }
-          ]
-        },
-        "code": {
-          "kwargs": {
-            "lifetime": 600
-          }
-        },
-        "token": {
-          "class": "oidcop.token.jwt_token.JWTToken",
-          "kwargs": {
-            "lifetime": 3600,
-            "add_claims": [
-              "email",
-              "email_verified",
-              "phone_number",
-              "phone_number_verified"
-            ],
-            "add_claim_by_scope": True,
-            "aud": [
-              "https://example.org/appl"
-            ]
-          }
-        },
-        "refresh": {
-          "kwargs": {
-            "lifetime": 86400
-          }
-        }
-      },
+      "token_handler_args": OIDC_OP_TOKEN_HANDLER,
       "userinfo": {
         "class": "oidc_provider.users.UserInfo",
         "kwargs": {
